8c620c392c110643eaa92307faa23c88c90e6e07
root <okwuosahpaschal@gmail.com>
2020-04-18 02:17:40 +0000

    Updates

---
+++ precache-manifest.a7850090c409606f246d99f19b47f8a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4049a4e20562049d95de9c6366f617a5",
+    "url": "/realtime-chatapp/index.html"
+  },
+  {
+    "revision": "b208dca59ad6fdf57847",
+    "url": "/realtime-chatapp/static/css/main.7743357d.chunk.css"
+  },
+  {
+    "revision": "ecd0df474675aa370ff2",
+    "url": "/realtime-chatapp/static/js/2.e2fdfa70.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/realtime-chatapp/static/js/2.e2fdfa70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b208dca59ad6fdf57847",
+    "url": "/realtime-chatapp/static/js/main.7df151d7.chunk.js"
+  },
+  {
+    "revision": "2f796b6607e699d5e2af",
+    "url": "/realtime-chatapp/static/js/runtime-main.82b0ffa9.js"
+  }
+]);
